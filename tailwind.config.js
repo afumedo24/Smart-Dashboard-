895d881f0b916c782f513e907f1d3081d863ea59
Abdaal Ahmed <ahmed.abdaal@stud.th-deg.de>
2024-04-20 17:16:27 +0200

    added_thd_colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.html',
-    './src/**/*.{vue,js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      thd_darkblue: '#1A4273',
+      thd_donaublue: '#009FE3',
+      thd_grey: '#6F6F6E',
+      thd_ai_red: 'E73D55',
+      white: '#ffffff',
+      black: '#1A1A1A'
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
