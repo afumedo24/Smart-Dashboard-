cce42ed0a60553edb55b06536cc668d5f870e7c0
Abdaal Ahmed <ahmed.abdaal@stud.th-deg.de>
2024-04-20 17:16:50 +0200

    created_NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <header>
-    <div class="">
-      <nav>
-        <ul>
-          <li>
-            <RouterLink to="/">Home</RouterLink>
-          </li>
-          <li>
-            <RouterLink to="/mensa">Mensa</RouterLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
-
- 
-
+  <NavBar />
   <RouterView />
 </template>
-
---
+++ src/components/NavBar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="w-full h-24 bg-thd_darkblue text-white">
+    <nav class="flex items-center">
+      <h1 class="font-bold mx-8 mt-4 p-1 ">LOGO</h1>
+      <ul class="">
+        <li class=""><router-link to="/">Home</router-link></li>
+        <li><router-link to="/mensa">About</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
