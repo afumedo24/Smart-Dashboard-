7ac4f8de580df85a61a01b7097de21531abb0912
Abdaal Ahmed <ahmed.abdaal@stud.th-deg.de>
2024-06-15 20:38:31 +0200

    cleaned up

--- src/stores/MensaStore.ts
+++ src/stores/MensaStore.ts
@@ -1,7 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { type Menu } from '../services/types/MensaMenu.type'
-import mensaInstance from '../services/axios/axios.config'
+import mensaInstance from '../services/axios/axios.config' // import the axios instance for the Mensa API
 
 export const useMensaStore = defineStore('mensa', () => {
   // this is the state of the store
@@ -13,7 +13,7 @@ export const useMensaStore = defineStore('mensa', () => {
   const getDate = computed(
     () => `${date.value.getDate()}.${date.value.getMonth() + 1}.${date.value.getFullYear()}`
   )
-
+  // this getter allows to get the date in the format that the API needs
   const getDateForAPI = computed(
     () => `${date.value.getFullYear()}-${date.value.getMonth() + 1}-${date.value.getDate()}`
   )
@@ -23,27 +23,26 @@ export const useMensaStore = defineStore('mensa', () => {
     try {
       const response = await mensaInstance.get<Menu[]>(`/${getDateForAPI.value}/meals`)
       menus.value = response.data
-      console.log('Menu fetched: ', response.data)
+      // console.log('Menu fetched: ', response.data) delete this later
     } catch (error) {
       console.error('Error fetching Menu: ', error)
       menus.value = []
     }
   }
-
+  // this action allows to set the date to a new date
   function setDate(newDate: Date) {
     date.value = newDate
   }
-
+  // this action allows to set the date to the previous day
   function setDateToPrevious() {
     const storedDate = date.value
     storedDate.setDate(storedDate.getDate() - 1)
-    console.log(storedDate)
     date.value = new Date(storedDate)
   }
+  // this action allows to set the date to the next day
   function setDateToNext() {
     const storedDate = date.value
     storedDate.setDate(storedDate.getDate() + 1)
-    console.log(storedDate)
     date.value = new Date(storedDate)
   }
 
