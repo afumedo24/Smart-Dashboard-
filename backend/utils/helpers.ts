ed81e85e6d13f333c11c63bfc787e921ce48fa33
Abdaal Ahmed <ahmed.abdaal@stud.th-deg.de>
2024-06-27 02:58:15 +0200

    added a setSensor helper

--- backend/utils/helpers.ts
+++ backend/utils/helpers.ts
@@ -1,6 +1,9 @@
 import crypto from 'crypto';
 import config from 'config';
 import jwt from 'jsonwebtoken';
+import fs from 'fs/promises';
+import lodash from 'lodash';
+import logger from './logger';
 
 // get all the configuration values from the config module
 const SALT = config.get<string>('salt');
@@ -38,4 +41,54 @@ export const unixToNormalTime = (unix: number) => {
 // a function that returns the URL of the weather icon
 export const weatherIconURL = (icon: string) => {
     return `https://openweathermap.org/img/wn/${icon}.png`;
+}
+
+
+let SENSORS_URI = '';
+
+const SENSORS_URI1 = config.get<string>('thd_sensor1_uri');
+const SENSORS_URI2 = config.get<string>('thd_sensor2_uri');
+
+
+
+export const setActiveSensor = async () => {
+    //Settings JSON file absolute path
+    const settingsFilePath = ((process.cwd()) + '/config/settings.json')
+
+    try {
+        const settingsFileContent = await fs.readFile(settingsFilePath, 'utf-8');
+        const settings = JSON.parse(settingsFileContent);
+
+        let activeSensors = lodash.filter(settings.sensors, (sensor) => lodash.get(sensor, 'isActive'));
+        
+        // Map over active sensors to add the URL
+        activeSensors = lodash.map(activeSensors, (sensor) => {
+            const sensorId = lodash.get(sensor, 'id');
+            logger.info(`Sensor ${sensorId} is active.`);
+            let sensorUrl: string;
+
+            if (sensorId === 1) {
+                sensorUrl = config.get<string>('thd_sensor1_uri');
+            } else if (sensorId === 2) {
+                sensorUrl = config.get<string>('thd_sensor2_uri');
+            } else {
+                sensorUrl = 'Unknown URL'; // Default or error URL
+                logger.error(`Unknown sensor ID: ${sensorId}`);
+            }
+
+            // Add URL to the sensor object
+            return {
+                ...sensor,
+                url: sensorUrl
+            };
+        });
+
+        // Log and return the active sensors with URLs
+        //console.log(activeSensors);
+        return activeSensors;
+
+    } catch (error: any) {
+        logger.error('Error reading settings file:', error.message)
+        return [];
+    }
 }
